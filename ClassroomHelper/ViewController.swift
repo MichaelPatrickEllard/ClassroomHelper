e9827e7e99c17ddadbbcf47307fdaae387845ece
Michael Patrick Ellard <ellard@rescuemissionsoftware.com>
2016-07-22 17:42:21 -0700

    Initial Commit

---
+++ ClassroomHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClassroomHelper
+//
+//  Created by Rescue Mission Software on 7/22/16.
+//  Copyright © 2016 RescueMissionSoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
