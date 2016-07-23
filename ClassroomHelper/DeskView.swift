1d8f2763841d586c08f67d0f71680ca20f7386f2
Michael Patrick Ellard <ellard@rescuemissionsoftware.com>
2016-07-23 12:22:46 -0700

    Adding ability to rotate desks

--- ClassroomHelper/DeskView.swift
+++ ClassroomHelper/DeskView.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class DeskView: UIView
 {
+    var referenceTransform = CGAffineTransformIdentity
+    
     override init(frame: CGRect)
     {
         super.init(frame: frame)
         self.backgroundColor = UIColor.blueColor()
-        addSwipeGesture()
+        addRotateGesture()
     }
     
     required init?(coder aDecoder: NSCoder)
@@ -22,13 +24,25 @@ class DeskView: UIView
         super.init(coder: aDecoder)
     }
     
-    func addSwipeGesture()
+    func addRotateGesture()
     {
+        let rotateGR = UIRotationGestureRecognizer(target: self, action: #selector(handleRotate))
         
+        self.addGestureRecognizer(rotateGR)
     }
     
-    func handleSwipe()
+    func handleRotate(gesture: UIRotationGestureRecognizer)
     {
+        if gesture.state == .Began
+        {
+            self.referenceTransform = self.transform
+        }
         
+        self.transform = CGAffineTransformRotate(referenceTransform, gesture.rotation)
+        
+        if gesture.state == .Ended
+        {
+            self.referenceTransform = self.transform
+        }
     }
 }
