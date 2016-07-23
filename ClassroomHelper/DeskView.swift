55bb71e156db708d5a8398bf6782665787dd91c4
Michael Patrick Ellard <ellard@rescuemissionsoftware.com>
2016-07-23 10:31:01 -0700

    Adding placeholder DeskView class

---
+++ ClassroomHelper/DeskView.swift
@@ -0,0 +1,33 @@
+//
+//  DeskView.swift
+//  ClassroomHelper
+//
+//  Created by Rescue Mission Software on 7/22/16.
+//  Copyright © 2016 RescueMissionSoftware. All rights reserved.
+//
+
+import UIKit
+
+class DeskView: UIView
+{
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        addSwipeGesture()
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+    }
+    
+    func addSwipeGesture()
+    {
+      
+    }
+    
+    func handleSwipe()
+    {
+        
+    }
+}
