13ce5248ff94ba6146cf9faa1bae2f2464d9597d
Michael Patrick Ellard <ellard@rescuemissionsoftware.com>
2016-07-25 07:02:49 -0700

    FINAL COMMIT BEFORE HACKATHON PRESENTATION
    Updating list of "students" for Hackathon presentation.

--- ClassroomHelper/Student.swift
+++ ClassroomHelper/Student.swift
@@ -27,11 +27,17 @@ class Student: NSObject
     {
         var students = [Student]()
         
-        students.append(Student(name: "Winnie Chen", comments: "Picks up new topics fast."))
-        students.append(Student(name: "Mike Ellard", comments: "Thinks he's supposed to be teaching the class."))
-        students.append(Student(name: "Bruce Chou", comments: "Knows everyone in the class."))
-        students.append(Student(name: "Ada Lovelace", comments: "Mathematics as Poetry."))
         students.append(Student(name: "Jacob Dudney", comments: "Great UX Designer."))
+        students.append(Student(name: "Bill Dudney", comments: "Mathematical Genius"))
+        students.append(Student(name: "Andrew Dudney", comments: "The apple falls not far from the tree."))
+        students.append(Student(name: "Steve Jobs Jr.", comments: "The Steve Jobs, Jr.?"))
+        students.append(Student(name: "Sharada Bose", comments: "She knows the Way!"))
+        students.append(Student(name: "Sundeep", comments: "Mathematics as Poetry."))
+        students.append(Student(name: "Eric Oesterle", comments: "Great guy to hang out with at iOS Dev Camp."))
+        students.append(Student(name: "Dom Sagolla", comments: "Master of Ceremonies"))
+        students.append(Student(name: "Jennifer Holmes", comments: "Master of the House."))
+        students.append(Student(name: "Paul Blair", comments: "Master of the Blue Tickets."))
+        students.append(Student(name: "Ada Lovelace", comments: "Mathematics as Poetry."))
         students.append(Student(name: "Grace Hopper", comments: "Get her to show you her millisecond."))
         students.append(Student(name: "Chris Lattner", comments: "He's a pretty swift guy."))
         students.append(Student(name: "Nikola Tesla", comments: "Shouldn't be seated next to Edison."))
@@ -39,6 +45,11 @@ class Student: NSObject
         students.append(Student(name: "Thomas Edison", comments: "When asked about his homework, says, 'I've tried 999 ways that didn't work."))
         students.append(Student(name: "Steve Wozniak", comments: "Doesn't trust a computer unless he can throw it out the window.  Didn't trust his computer.  Threw it out the window."))
         students.append(Student(name: "Alan Turing", comments: "Seems a little wooden sometimes. Never quite sure if he's a person or a computer."))
+        students.append(Student(name: "Mike Ellard", comments: "Thinks he's supposed to be teaching the class."))
+        students.append(Student(name: "Winnie Chen", comments: "Picks up new topics fast."))
+        students.append(Student(name: "Bruce Chou", comments: "Knows everyone in the class."))
+        
+
     
         return students
     }
